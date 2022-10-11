a35a56787baae2a34d651b1935499df31a2a7aed
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-10-11 15:47:16 +0100

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Stopwatch;
+
+var stopWatch = new StopWatch();
+var currentState = false;
+
+while (true)
+{
+    if (!currentState)
+        Console.WriteLine("Type s to Start,\n x to Stop \n r to Repeat \n e to Exit \n ");
+
+    var userInput = Console.ReadLine();
+
+    if (userInput.ToLower().Equals("s"))
+    {
+        stopWatch.Start();
+        Console.WriteLine("StopWatch has started' \n x to Stop \n r to Repet \n any other key to Exit\n ");
+        currentState = true;
+    }
+
+    if (userInput.ToLower().Equals("x"))
+    {
+        stopWatch.Stop();
+        Console.WriteLine("The Total Time Span calculated was -> " + stopWatch.GetInterval());
+        currentState = false;
+    }
+
+    if (userInput.ToLower().Equals("r"))
+    {
+        currentState = false;
+        Console.WriteLine("StopWatch has been Restarted");
+        continue;
+    }
+
+    if (!userInput.ToLower().Equals("s") && !userInput.ToLower().Equals("x") && !userInput.ToLower().Equals("r"))
+    {
+        break;
+    }
+}
---
+++ Stopwatch/StopWatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    // The job of this class is to simulate a stopwatch. It provide two methods: Which is the Start and Stop.
+    // We Would call the start method first, and the stop method next. 
+    // Then we ask the stopwatch about the duration between the start and stop period.
+    // The Duration would be a value in TimeSpan.
+    // Then i display the duration onto the console.
+
+
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _isRunning;
+
+        // Here is the Start Method
+        public void Start()
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("StopWatch is already running.");
+
+            _isRunning = true;
+            _startTime = DateTime.Now;
+
+        }
+
+        // And here is alsothree stop methid
+        public void Stop()
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("StopWatch is not running.");
+            _isRunning = false;
+            _endTime = DateTime.Now;
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return _endTime - _startTime;
+        }
+    }
+}
